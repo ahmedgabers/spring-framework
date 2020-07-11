15e7fab72b181ab8c98ad45e36e57d1dbb1acd23
x300n <houseofxeon@gmail.com>
2020-07-11 20:27:44 +0200

    Updates

---
+++ src/com/ahmedgaber/annotations/DatabaseFortuneService.java
@@ -0,0 +1,13 @@
+package com.ahmedgaber.annotations;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return null;
+	}
+
+}
