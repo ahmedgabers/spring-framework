944b69e270bc012ac3abaa68e19c26e638342ca1
x300n <houseofxeon@gmail.com>
2020-07-11 21:00:34 +0200

    Updates

---
+++ src/com/ahmedgaber/annotations/SportConfig.java
@@ -0,0 +1,11 @@
+package com.ahmedgaber.annotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ahmedgaber.annotations")
+public class SportConfig {
+
+		
+}
