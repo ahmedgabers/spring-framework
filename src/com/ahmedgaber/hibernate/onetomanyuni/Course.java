90dab5ee1414ddc6c5fe1f47cedc53445a88674e
x300n <houseofxeon@gmail.com>
2020-07-13 19:33:38 +0200

    Updates

--- src/com/ahmedgaber/hibernate/onetomanyuni/Course.java
+++ src/com/ahmedgaber/hibernate/onetomanyuni/Course.java
@@ -1,13 +1,18 @@
 package com.ahmedgaber.hibernate.onetomanyuni;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,13 +31,25 @@ public class Course {
 							CascadeType.PERSIST, CascadeType.REFRESH })
 	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
-
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="course_id")
+	private List<Review> reviews;
+	
 	public Course() {
 
 	}
 
 	public Course(String title) {
 		this.title = title;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
 	}
 
 	public int getId() {
@@ -57,6 +74,14 @@ public class Course {
 
 	public void setInstructor(Instructor instructor) {
 		this.instructor = instructor;
+	}
+	
+	public void add(Review theReview) {
+		if(reviews == null) {
+			reviews = new ArrayList<>();
+		}
+		
+		reviews.add(theReview);
 	}
 
 	@Override
---
+++ src/com/ahmedgaber/hibernate/onetomanyuni/DeleteCoursesAndReviews.java
@@ -0,0 +1,48 @@
+package com.ahmedgaber.hibernate.onetomanyuni;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteCoursesAndReviews {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration().configure().addAnnotatedClass(Instructor.class)
+												    .addAnnotatedClass(InstructorDetail.class)
+												    .addAnnotatedClass(Course.class)
+												    .addAnnotatedClass(Review.class)
+												    .buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			
+			
+			session.beginTransaction();
+			int theId = 10;
+			
+			Course tempCourse = session.get(Course.class, theId);
+			System.out.println("Deleting the course .....");
+			System.out.println(tempCourse);
+			System.out.println(tempCourse.getReviews());
+			
+			session.delete(tempCourse);
+			session.getTransaction().commit();
+			
+			System.out.println("Done! ");
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.close();
+			factory.close();
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
