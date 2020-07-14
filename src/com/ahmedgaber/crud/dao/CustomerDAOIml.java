876a4a5643e700ca853378bfddf465c48ea6f824
x300n <houseofxeon@gmail.com>
2020-07-14 21:37:33 +0200

    Updates

--- src/com/ahmedgaber/crud/dao/CustomerDAOIml.java
+++ src/com/ahmedgaber/crud/dao/CustomerDAOIml.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.ahmedgaber.crud.entity.Customer;
 
+@Repository
 public class CustomerDAOIml implements CustomerDAO {
 
 	
@@ -20,7 +24,14 @@ public class CustomerDAOIml implements CustomerDAO {
 	@Transactional
 	public List<Customer> getCustomers() {
 		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
 		// create a query and get result list
+		
+		Query<Customer> theQuery = 
+				currentSession.createQuery("from Customer", Customer.class);
+		
+		List<Customer> customers = theQuery.getResultList();
 		
 		return null;
 	}
