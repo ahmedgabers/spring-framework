37b8db66930ce01067f7e6887498861ce357df46
x300n <houseofxeon@gmail.com>
2020-07-17 22:03:00 +0200

    Updates

--- springREST-crm/src/main/java/org/ahmedgaber/springrest/rest/CustomerRestController.java
+++ springREST-crm/src/main/java/org/ahmedgaber/springrest/rest/CustomerRestController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.ahmedgaber.springrest.entity.Customer;
 import org.ahmedgaber.springrest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +35,37 @@ public class CustomerRestController {
 	
 			return theCustomer;
 		}
+		
+		@PostMapping("/customers")
+		public Customer addCustomer(@RequestBody Customer theCustomer) {
+			
+			theCustomer.setId(0);
+			
+			
+			customerService.saveCustomer(theCustomer);
+			return theCustomer;
+		}
+		
+		@PutMapping("/customers")
+		public Customer updateCustomer(@RequestBody Customer theCustomer) {
+			customerService.saveCustomer(theCustomer);
+			
+			return theCustomer;
+		}
+		
+		@DeleteMapping("/customers/{customerId}")
+		public String deleteCustomer(@PathVariable int customerId) {
+			
+			
+			Customer theCustomer = customerService.getCustomer(customerId);
+			
+			if(theCustomer == null) throw new CustomerNotFoundException("Customer id not found - " + customerId);			
+			
+			customerService.deleteCustomer(customerId);
+			
+			return theCustomer.getFirstName() + " has been deleted";
+			
+		}
+		
 
 }
