e572930192d05fa75e9c66c4ef684c3a464b69da
x300n <houseofxeon@gmail.com>
2020-07-17 21:45:39 +0200

    Updates

--- springREST-crm/src/main/java/org/ahmedgaber/springrest/rest/CustomerRestController.java
+++ springREST-crm/src/main/java/org/ahmedgaber/springrest/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.ahmedgaber.springrest.entity.Customer;
 import org.ahmedgaber.springrest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,16 @@ public class CustomerRestController {
 		@GetMapping("/customers")
 		public List<Customer> getCustomers() {
 			return customerService.getCustomers();
+		}
+		
+		@GetMapping("/customers/{customerId}")
+		public Customer getCustomer(@PathVariable int customerId) {
+			
+			Customer theCustomer = customerService.getCustomer(customerId);
+			
+			if(theCustomer == null) throw new CustomerNotFoundException("Customer id not found - " + customerId);
+	
+			return theCustomer;
 		}
 
 }
